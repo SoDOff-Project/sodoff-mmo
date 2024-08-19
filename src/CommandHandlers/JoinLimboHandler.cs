7d96a030427b6726edefbd021d684b7faa679305
rpaciorek <robert@opcode.eu.org>
2024-08-19 21:19:48 +0200

    implement JL (Join LIMBO) command
    Join Limbo command based on Alan Moon PR
    
    ---------
    
    Co-authored-by: Alan Moon <alanmoonbase2004@gmail.com>

---
+++ src/CommandHandlers/JoinLimboHandler.cs
@@ -0,0 +1,17 @@
+﻿using sodoffmmo.Attributes;
+using sodoffmmo.Core;
+using sodoffmmo.Data;
+
+namespace sodoffmmo.CommandHandlers
+{
+    [ExtensionCommandHandler("JL")]
+    public class JoinLimboHandler : CommandHandler
+    {
+        public override Task Handle(Client client, NetworkObject receivedObject)
+        {
+            client.SetRoom(Room.GetOrAdd("LIMBO"));
+
+            return Task.CompletedTask;
+        }
+    }
+}
