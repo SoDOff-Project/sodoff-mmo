c06cfb4d1711891a8c43a3a029cb222ea30ae6b7
Hipposgrumm <60556345+Hipposgrumm@users.noreply.github.com>
2024-12-29 09:05:32 -0700

    Support for Eat My Dust (#4)
    * Support for Eat My Dust
    Zombie Alert manager is stubbed.
    Added helpers to allow for weapon MMO.
    PlayerData for Eat My Dust
    
    * Zombie Alert Added
    * Fix Zombie Alert Timing
    * Honestly should probably catch this exception [null room in PM handler]. I foresee it occurring very commonly. While it shouldn't halt the server (except in visual studio debug), it's annoying to see.

---
+++ src/CommandHandlers/EMDZombiesHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using sodoffmmo.Attributes;
+using sodoffmmo.Core;
+using sodoffmmo.Data;
+
+namespace sodoffmmo.CommandHandlers;
+
+// TODO: This is currently stubbed. Supposed to do something.
+// Should probably be done by someone who actually played the game.
+[ExtensionCommandHandler("SU")]
+public class EMDZombiesUpdateHandler : CommandHandler {
+    public override Task Handle(Client client, NetworkObject receivedObject) {
+        return Task.CompletedTask;
+    }
+}
+
+[ExtensionCommandHandler("EN")]
+public class EMDZombiesEnterHandler : CommandHandler {
+    public override Task Handle(Client client, NetworkObject receivedObject) {
+        return Task.CompletedTask;
+    }
+}
+
+[ExtensionCommandHandler("EX")]
+public class EMDZombiesExitHandler : CommandHandler {
+    public override Task Handle(Client client, NetworkObject receivedObject) {
+        return Task.CompletedTask;
+    }
+}
