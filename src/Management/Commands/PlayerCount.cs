98fa2742f4f7f7d2f91e0c4acfceb945b6269d25
Spirtix <michal.prazak1@gmail.com>
2025-06-05 10:41:56 +0200

    player count management command

---
+++ src/Management/Commands/PlayerCount.cs
@@ -0,0 +1,16 @@
+﻿using sodoffmmo.Attributes;
+using sodoffmmo.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sodoffmmo.Management.Commands;
+
+[ManagementCommand("playercount", Role.Admin)]
+class PlayerCount : IManagementCommand {
+    public void Handle(Client client, string[] arguments) {
+        client.Send(Utils.BuildServerSideMessage($"Current room: {(client.Room?.ClientsCount.ToString() ?? "not in room")}, Server total: {Room.AllRooms().Sum(x => x.ClientsCount)}", "Server"));
+    }
+}
