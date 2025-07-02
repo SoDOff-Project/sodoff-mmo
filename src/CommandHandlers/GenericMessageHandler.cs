a198bd8e9e96dd4c94bb201805c7783534b6ebfd
Spirtix <michal.prazak1@gmail.com>
2025-07-02 21:22:35 +0200

    disable canned chat when chat is disabled globally

--- src/CommandHandlers/GenericMessageHandler.cs
+++ src/CommandHandlers/GenericMessageHandler.cs
@@ -7,6 +7,8 @@ namespace sodoffmmo.CommandHandlers;
 [CommandHandler(7)]
 class GenericMessageHandler : CommandHandler {
     public override Task Handle(Client client, NetworkObject receivedObject) {
+        if (!Configuration.ServerConfiguration.EnableChat && receivedObject.Get<string>("m").StartsWith("C:"))
+            return Task.CompletedTask;
         NetworkPacket packet = NetworkObject.WrapObject(0, 7, receivedObject).Serialize();
         client.Room.Send(packet);
         return Task.CompletedTask;
